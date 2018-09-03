ec0fd5dc1524fdfcc5fc1004030735095ce695b4
ludwig.vantours <ludwig.vantours@ext.cdiscount.com>
2018-09-03 13:36:12 +0200

    test styled-components

--- App.js
+++ App.js
@@ -5,11 +5,20 @@ import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 import reducer from './reducers';
 import History from './components/History';
+// import styled from 'styled-components/native';
 
 const store = createStore(
   reducer /* preloadedState, */,
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 );
+// exemple styledComponents
+// const CenterView = styled.View`
+//   flex: 1;
+//   align-items: center;
+//   justify-content: center;
+//   background: #333;
+// `;
+
 export default class App extends React.Component {
   render() {
     return (
