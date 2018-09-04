3f6085cab3033cf3a35e94f2b61abb25bafa7573
ludwig.vantours <ludwig.vantours@ext.cdiscount.com>
2018-09-04 10:11:53 +0200

    Connect EntryDetail

--- components/EntryDetail.js
+++ components/EntryDetail.js
@@ -1,5 +1,17 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
+import { connect } from 'react-redux';
+import { white } from '../utils/colors';
+import MetricCard from './MetricCard';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: white,
+    padding: 15,
+  },
+});
+
 class EntryDetail extends Component {
   static navigationOptions = ({ navigation }) => {
     const { entryId } = navigation.state.params;
@@ -13,15 +25,24 @@ class EntryDetail extends Component {
     };
   };
   render() {
+    const { entryId } = this.props.navigation.state.params;
+    const { metrics } = this.props;
+
     return (
-      <View>
-        <Text>
-          Entry Detail -{' '}
-          {JSON.stringify(this.props.navigation.state.params.entryId)}
-        </Text>
+      <View style={styles.container}>
+        <MetricCard date={entryId} metrics={metrics} />
+        <Text>Entry Detail - {JSON.stringify(this.props.metrics)}</Text>
       </View>
     );
   }
 }
 
-export default EntryDetail;
+const mapStateToProps = (state, { navigation }) => {
+  const { entryId } = navigation.state.params;
+  return {
+    entryId,
+    metrics: state[entryId],
+  };
+};
+
+export default connect(mapStateToProps)(EntryDetail);
